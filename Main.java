b1d78bc6b09e37e1dd77e9ed49e0df4eb3629fea
mulsannestraight <22200664+mulsannestraight@users.noreply.github.com>
2021-03-09 23:10:34 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+       TTT game1 = new TTT();
+       game1.displayMenu();
+       game1.startGame();
+    }
+}
